bc9800ef915066a7fb8c97af6f212385f1a2f298
Paul <paul.guerra@gmail.com>
2018-08-28 16:21:20 -0600

    Subir imagenes de usuarios y productos

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,10 +2,21 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
+
+const fs = require('fs');
+const path = require('path');
+
 // default options
 app.use(fileUpload());
 
-app.put('/upload', function(req, res) {
+app.put('/upload/:tipo/:id', function(req, res) {
+
+    let tipo = req.params.tipo;
+    let id = req.params.id;
+
+
     if (!req.files) {
         return res.status(400).json({
             ok: false,
@@ -14,6 +25,18 @@ app.put('/upload', function(req, res) {
             }
 
         });
+    }
+
+    //Validar tipo
+    let tiposValidos = ['productos', 'usuarios'];
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos válidos son ' + tiposValidos.join(', '),
+                tipo: tipo
+            }
+        })
     }
 
     let archivo = req.files.archivo;
@@ -34,21 +57,115 @@ app.put('/upload', function(req, res) {
         })
     }
 
+    //Cambiar el nombre de archivo
+    let nombreArchivo = `${ id }-${ new Date().getMilliseconds() }.${ extension }`;
+
     // Use the mv() method to place the file somewhere on your server
-    archivo.mv(`uploads/${ archivo.name }`, (err) => {
+    archivo.mv(`uploads/${ tipo }/${ nombreArchivo }`, (err) => {
         if (err)
             return res.status(500).json({
                 ok: false,
                 err
             });
 
-        res.json({
-            ok: true,
-            message: 'Archivo cargado'
-        });
+        switch (tipo) {
+            case 'usuarios':
+                imagenUsuario(id, res, nombreArchivo);
+                break;
+            case 'productos':
+                imagenProducto(id, res, nombreArchivo);
+                break;
+        }
+
+        // res.json({
+        //     ok: true,
+        //     message: 'Archivo cargado'
+        // });
     });
 
 
 });
+
+
+function imagenProducto(id, res, nombreArchivo) {
+    Producto.findById(id, (err, productoDB) => {
+        if (err) {
+            borraArchivo(nombreArchivo, 'productos');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!productoDB) {
+            borraArchivo(nombreArchivo, 'productos');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Producto no existe'
+                }
+            })
+        }
+
+
+
+        borraArchivo(productoDB.img, 'productos');
+
+
+        productoDB.img = nombreArchivo;
+
+        productoDB.save((err, productoGuardado) => {
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            })
+        })
+
+    })
+}
+
+function imagenUsuario(id, res, nombreArchivo) {
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            borraArchivo(nombreArchivo, 'usuarios');
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (!usuarioDB) {
+            borraArchivo(nombreArchivo, 'usuarios');
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            })
+        }
+
+
+
+        borraArchivo(usuarioDB.img, 'usuarios');
+
+
+        usuarioDB.img = nombreArchivo;
+
+        usuarioDB.save((err, usuarioGuardado) => {
+            res.json({
+                ok: true,
+                producto: usuarioGuardado,
+                img: nombreArchivo
+            })
+        })
+
+    })
+}
+
+function borraArchivo(nombreImagen, tipo) {
+    let pathImagen = path.resolve(__dirname, `../../uploads/${ tipo }/${ nombreImagen }`);
+    if (fs.existsSync(pathImagen)) {
+        fs.unlinkSync(pathImagen);
+    }
+}
 
 module.exports = app;
